5e760853d00706a03ac69625ee74fcf33947228c
certified-draco <successolamide46@gmail.com>
2022-08-31 13:41:52 +0100

    Initial Commmit��

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+@app.route('/')
+def index():
+    # show all todos
+    todo_list = Todo.query.all()
+    print(todo_list)
+
+
+    return render_template('base.html', todo_list=todo_list)    
+
+# @app.route('/about')
+#def about():
+#    return "About Me!"
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
