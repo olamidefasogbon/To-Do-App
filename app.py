674371ad9abba40828c6003b3e14b975a672b85b
root <root@DESKTOP-FCBDDF4>
2022-08-31 16:09:50 +0100

    Initial Commit🎉🎉

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+# Import flask and create the application object
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,24 +7,47 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+#DtaBase
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
+#Routes
 @app.route('/')
 def index():
     # show all todos
-    todo_list = Todo.query.all()
-    print(todo_list)
+    todo_list = Todo.query.all() 
+    return render_template('base.html', todo_list=todo_list)
+
+@app.route('/add', methods=['post'])
+def add():
+    # add a new todo item
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
-    return render_template('base.html', todo_list=todo_list)    
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    # update unique todo item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for('index'))
 
-# @app.route('/about')
-#def about():
-#    return "About Me!"
 
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    # delete unique todo item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+# Custom
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
